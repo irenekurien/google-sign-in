fbdd724d8035a7f3e13034c44e915d2122fe71b4
Irene <irenekurien01@gmail.com>
2020-11-11 22:27:30 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:auth_app/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aion-Authentication-Task',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
